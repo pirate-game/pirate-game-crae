9194386bcdd535a958f09989f9f94846960a95b9
The Pirate King <60094408+pirate-game@users.noreply.github.com>
2020-11-21 16:11:44 +0000

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -55,7 +55,7 @@ const mainPageContent = <React.Fragment>
 
 const defaultLoading = <div>YARR! This be loadin'...</div>;
 
-const rulesContent_impl = React.lazy(() => import("./rules.js"));								     
+const rulesContent_impl = React.lazy(() => import("%PUBLIC_URL%/rules.js"));
 const rulesContent = <React.Suspense fallback={defaultLoading}><rulesContent_impl /></React.Suspense>;
 
 const toRender = <React.Fragment>
