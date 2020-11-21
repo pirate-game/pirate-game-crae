c9c6096edf4fe436b9d580dc368dd7bdbfc102e6
The Pirate King <60094408+pirate-game@users.noreply.github.com>
2020-11-21 14:47:39 +0000

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const navbar = <React.Fragment>
+const navbar = <div id="nav">
 	<ul>
       		<li><button id="logo"><img border="0" src="logo.png" /></button></li>
 		<li><button>Start&nbsp;a<br />Game</button></li>
@@ -10,10 +10,14 @@ const navbar = <React.Fragment>
 		<li><button>Acknowledgements</button></li>
 	</ul>
 	<h1 id="title">&nbsp;The&nbsp;Pirate&nbsp;Game&nbsp;</h1>
-</React.Fragment>;
+</div>;
+
+const titlebar = <div id="titlebar">
+	<h1 id="title" style={{fontSize: '50px'}}>The&nbsp;Pirate&nbsp;Game</h1>
+</div>;
 
 const toRender = <React.Fragment>
-	<div id="nav">{navbar}</div>
+	<div id="navortitlebar">{navbar}{titlebar}</div>
 </React.Fragment>;
 
 ReactDOM.render(toRender, document.getElementById('root'));
