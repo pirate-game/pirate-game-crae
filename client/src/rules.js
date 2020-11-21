56a423a7c54f1cbfe11564e2fbe3362772e3adcb
The Pirate King <60094408+pirate-game@users.noreply.github.com>
2020-11-21 20:48:56 +0000

    Update rules.js

--- client/src/rules.js
+++ client/src/rules.js
@@ -2,6 +2,4 @@ import React from 'react';
 
 console.log("I WAS IMPORTED!");
 
-const impl = () => <React.Fragment><p>RULES RULES RULES!!!</p></React.Fragment>;
-
-export default impl;
+export default function() { return <React.Fragment><p>RULES RULES RULES!!!</p></React.Fragment>; };
