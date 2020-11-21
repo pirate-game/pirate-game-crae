86ad4465727f44b41f78b9a8ad790e17e2cd0926
The Pirate King <60094408+pirate-game@users.noreply.github.com>
2020-11-21 13:24:18 +0000

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -4,4 +4,4 @@ import ReactDOM from 'react-dom';
 //function test() { ReactDOM.render(<p>Second phrase!</p>, document.getElementById('root')); };
 
 //ReactDOM.render(<div><p>Hello, World!</p><button onClick={test}>Click Me!</button></div>, document.getElementById('root'));
-//ReactDOM.render(<p>Second phrase!</p>,  document.getElementById('root'));
+ReactDOM.render(<p>Second phrase!</p>,  document.getElementById('root'));
