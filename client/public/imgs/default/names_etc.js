0c649f652eb20b6d3c892741748cd10449028124
The Pirate King <60094408+pirate-game@users.noreply.github.com>
2020-11-24 17:12:57 +0000

    Create names_etc.js

---
+++ client/public/imgs/default/names_etc.js
@@ -0,0 +1,17 @@
+export default {
+  "rob": {
+    "filename": "imgs/default/rob.png",
+    "name": "Rob",
+    "description": "Allows you to take all of another player's CASH and add it to your own. Their Bank balance is safe from this. That you robbed them is announced to all players but how much CASH you took is kept secret."
+  },
+  "kill": {
+    "filename": "imgs/default/kill.svg",
+    "name": "Kill",
+    "description": "Allows you to reduce another player's CASH to zero. Their Bank balance is safe from this. This is announced to all players. That player is not out of the game, they must just start again from zero CASH."
+  },
+  "present": {
+    "filename": "imgs/default/present.png",
+    "name": "Present",
+    "description": "Allows you to increase another player's CASH by 1000. This is announced to all players."
+  }
+}; // and so on and so forth...
