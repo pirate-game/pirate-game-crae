07d5f07d2d9c873ccac5da1defbf32f90140563c
The Pirate King <60094408+pirate-game@users.noreply.github.com>
2020-11-28 14:43:07 +0000

    Update names_etc.js

--- client/public/imgs/default/names_etc.js
+++ client/public/imgs/default/names_etc.js
@@ -1,17 +1,57 @@
 export default {
   "rob": {
-    "filename": "imgs/default/rob.png",
+    "file_ext": ".png",
     "name": "Rob",
     "description": "Allows you to take all of another player's CASH and add it to your own. Their Bank balance is safe from this. That you robbed them is announced to all players but how much CASH you took is kept secret."
   },
   "kill": {
-    "filename": "imgs/default/kill.svg",
+    "file_ext": ".svg",
     "name": "Kill",
     "description": "Allows you to reduce another player's CASH to zero. Their Bank balance is safe from this. This is announced to all players. That player is not out of the game, they must just start again from zero CASH."
   },
   "present": {
-    "filename": "imgs/default/present.png",
+    "file_ext": ".png",
     "name": "Present",
     "description": "Allows you to increase another player's CASH by 1000. This is announced to all players."
+  },
+  "declareScore": {
+    "file_ext": ".png",
+    "name": "Gobby Parrot",
+    "description": "Announces your CASH to all players."
+  },
+  "swap": {
+    "file_ext": ".png",
+    "name": "Swap",
+    "description": "Allows you to swap CASH with another player. Both your Bank balances are safe from this. That you swapped with each other is announced to all players but how much CASH each of you had is kept secret."
+  },
+  "chooseNextSquare": {
+    "file_ext": ".png",
+    "name": "Choose Next Square",
+    "description": "Adds you to a queue of people who will get to choose the next square. All players can see this queue."
+  },
+  "shield": {
+    "file_ext": ".svg",
+    "name": "Shield",
+    "description": "Block the bad! The Shield is placed in your 'Shield' box and you can choose to use it to block the bad at a later time. Nobody but you knows whether you have a Shield."
+  },
+  "mirror": {
+    "file_ext": ".png",
+    "name": "Mirror",
+    "description": "Reflect the bad! The Mirror is placed in your 'Mirror' box and you can choose to use it to reflect the bad back at the attacker at a later time. For example, if you were being robbed, you could mirror it and rob them instead. They could use their Shield or Mirror at this point. Nobody but you knows whether you have a Mirror."
+  },
+  "goToZero": {
+    "file_ext": ".svg",
+    "name": "Bomb",
+    "description": "YOU go to zero! Your CASH goes to zero. Your Bank balance is safe from this. Nobody knows when this happens."
+  },
+  "double": {
+    "file_ext": ".svg",
+    "name": "Double",
+    "description": "Your CASH doubles. Your Bank balance is not affected. Nobody knows when this happens."
+  },
+  "bank": {
+    "file_ext": ".svg",
+    "name": "Bank",
+    "description": "Your CASH moves into your Bank."
   }
-}; // and so on and so forth...
+};
