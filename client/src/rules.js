6c5e40a894d0f69fbbd1307cdf9bbcd3bcfdbaef
The Pirate King <60094408+pirate-game@users.noreply.github.com>
2020-11-28 15:54:26 +0000

    Update rules.js

--- client/src/rules.js
+++ client/src/rules.js
@@ -3,7 +3,7 @@ import React from 'react';
 import shared_vars from './shared_vars';
 
 export default () => (
-<div class="sometext" style="background-color:lightblue;">
+<div className="sometext" style={{backgroundColor:"lightblue"}}>
     <p>Hello!</p>
 </div>
 );
