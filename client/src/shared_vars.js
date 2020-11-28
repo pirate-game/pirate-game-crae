02cefe0c5b02375c987b40024b68ac7aba8a8eb7
The Pirate King <60094408+pirate-game@users.noreply.github.com>
2020-11-28 16:35:56 +0000

    Update shared_vars.js

--- client/src/shared_vars.js
+++ client/src/shared_vars.js
@@ -6,6 +6,8 @@ let shared_vars = {
   "theme": "default",
   "currentPlace": "index",
   "pushState": pushState,
+  "forward": () => shared_vars.pushState(shared_vars.currentPlace),
+  "back": (ev) => shared_vars.gotoPage[ev.state](),
   "gotoPage": {}
 };
 export default shared_vars;
