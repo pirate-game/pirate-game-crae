8053e858df3029fc28543d0c8f06ecadb2a6715e
The Pirate King <60094408+pirate-game@users.noreply.github.com>
2020-11-28 15:19:37 +0000

    Update rules.js

--- client/src/rules.js
+++ client/src/rules.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { x } from './shared_vars';
+import shared_vars from './shared_vars';
 
-export function f(){ return x; };
+export function f(){ return shared_vars.x; };
 
 export default () => <React.Fragment><p>RULES RULES RULES!!!</p></React.Fragment>;
