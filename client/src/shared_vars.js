35a7e2a30c0307daee7f18003ede640f8858d768
The Pirate King <60094408+pirate-game@users.noreply.github.com>
2020-11-28 16:24:22 +0000

    Update shared_vars.js

--- client/src/shared_vars.js
+++ client/src/shared_vars.js
@@ -1,4 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function pushState(state) {
+  window.history.pushState(state, "");
+};
+
 let shared_vars = {
-  "theme": "default"
+  "theme": "default",
+  "pushState": pushState,
+  "gotoPage": {}
 };
 export default shared_vars;
