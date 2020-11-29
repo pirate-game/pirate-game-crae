184d7110ab31e683ad90b42b80a89ef804fafc06
The Pirate King <60094408+pirate-game@users.noreply.github.com>
2020-11-29 19:53:12 +0000

    Create submitted.js

---
+++ client/src/submitted.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import shared_vars from './shared_vars';
+
+export default class Submitted extends React.Component {
+  render() {
+    return <div className="sometext" style={{backgroundColor:"lightblue"}}>
+            		<p>Thank you! Form submitted. Hopefully your form made it to us.</p>
+        	</div>;
+  };
+};
