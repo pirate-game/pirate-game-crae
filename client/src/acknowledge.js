63cccd14cc93788abadfded4ee6b0971dd66aa4d
The Pirate King <60094408+pirate-game@users.noreply.github.com>
2020-11-29 18:44:53 +0000

    Update acknowledge.js

--- client/src/acknowledge.js
+++ client/src/acknowledge.js
@@ -6,11 +6,20 @@ import './css/acknowledge.css';
 
 export default class Acknowledge extends shared_vars.ThemeDependentComponent {
   render() {
-	  const content = null;
+	  let content = shared_vars.defaultLoading;
+	  if (this.state.data) {
+	  	content = <p style={{marginLeft:"4em", textIndent:"-4em"}}>
+				Coming soon...
+			</p>;
+	  };
     return <React.Fragment>
         <div className="sometext" style={{backgroundColor:"lightblue"}}>
-          <p style={{marginLeft:"4em", textIndent:"-4em"}}>
-            Stuff!
+          <p style={{marginLeft:"4em", textIndent:"-4em"}}>We here at The Pirate Game would like to thank:<br />
+            <a href="https://pages.github.com/">GitHub Pages</a> for originally hosting our lovely website<br />
+	    <a href="https://www.heroku.com/home">Heroku</a> for hosting it in all its glory with Node.js etc.<br />
+            <a href="https://formsubmit.io/">FormSubmit.io</a> for handling our forms<br />
+            <a href="https://www.tes.com/teaching-resource/the-pirate-game-end-of-term-activity-6258063">paulcollins</a> for providing the idea for the Pirate Game and allowing us to use it under the Tes Classic Free Licence<br />
+            <a href="https://www.tes.com/teaching-resource/the-pirate-game-square-picker-6437335">htmort</a> for providing the 'Square Picker' and allowing us to use it with attribution under the Creative Commons "Sharealike"<br />
           </p>
           {content}
           <p>
