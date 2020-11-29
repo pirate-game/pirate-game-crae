a4cbbbdfb484d6379dfe433e909520f9291a9f36
The Pirate King <60094408+pirate-game@users.noreply.github.com>
2020-11-29 12:23:42 +0000

    Update shared_vars.js

--- client/src/shared_vars.js
+++ client/src/shared_vars.js
@@ -3,6 +3,7 @@ import React from 'react';
 let shared_vars = {
   "theme": "default",
   "gotoPage": {},
-  "defaultLoading": <div>YARR! This be loadin'...</div>
+  "defaultLoading": <div>YARR! This be loadin'...</div>,
+  "symbols": ["rob", "kill", "present", "declareScore", "swap", "chooseNextSquare", "shield", "mirror", "goToZero", "double", "bank"]
 };
 export default shared_vars;
