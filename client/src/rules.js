a026e994641ac0916854184b579c06a4cc15f0bd
The Pirate King <60094408+pirate-game@users.noreply.github.com>
2020-11-29 12:30:11 +0000

    Update rules.js

--- client/src/rules.js
+++ client/src/rules.js
@@ -2,11 +2,55 @@ import React from 'react';
 
 import shared_vars from './shared_vars';
 
-export default () => (
-<div className="sometext" style={{backgroundColor:"lightblue"}}>
-    <React.Suspense fallback={shared_vars.defaultLoading}>
-        <p>Hello!</p>
-        //Must be a class, to allow update upon change of theme
-    </React.Suspense>
-</div>
-);
+function intersperseWith(array, element) {
+    const len = array.length;
+    if (len) {
+        let out = [array[0]];
+        for (let i = 1; i < len; ++i) { // it is meant to start at 1, so that the first element is missed
+            out = out.concat([element, array[i]]);
+        };
+        return out;
+    } else {
+        return array;
+    };
+};
+
+export default class Rules extends React.Component {
+    constructor() {
+        super();
+        this.state = { data: null };
+        this.updateTheme();
+    };
+    updateTheme() {
+        const theme = shared_vars.theme;
+        import('imgs/'+theme+'/names_etc').then(d => {
+            if (shared_vars.theme == theme) {
+                this.setState({ data: d });
+            };
+        });
+    };
+    render() {
+        let content = null;
+        const data = this.state[data];
+        if (data === null) {
+            content = shared_vars.defaultLoading;
+        } else {
+            content = <React.Fragment>
+                <p>{intersperseWith(data.rules_preamble, <React.Fragment><br /><br /></React.Fragment>)}</p>
+                <div id="symbols">
+                    {shared_vars.symbols.map(symbol => {
+                        const sym = data[symbol];
+                        return <div>
+					            <div class="square"><img src={"imgs/"+shared_vars.theme+"/"+symbol+sym.file_ext} /></div>
+					            <p>{sym.name+": "+sym.description}</p>
+				            </div>;}
+                        )}
+                </div>
+                <p>The winner is the pirate whose final CASH and Bank balance total the most.</p>
+            </React.Fragment>;
+        };
+        return <div className="sometext" style={{backgroundColor:"lightblue"}}>
+            {content}
+        </div>;
+    };
+};
