ba4d35ed3fcdb933cd515899a6fd223831e2053c
The Pirate King <60094408+pirate-game@users.noreply.github.com>
2020-11-29 17:53:14 +0000

    Create acknowledge.js

---
+++ client/src/acknowledge.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import shared_vars from './shared_vars';
+
+export default class Acknowledge extends shared_vars.ThemeDependentComponent {
+  render() {
+    return <p>Acknowledge</p>;
+  };
+};
