6ca09406e8baa14838b6e4081ff3d745ddf3a44e
The Pirate King <60094408+pirate-game@users.noreply.github.com>
2020-11-29 13:44:03 +0000

    Update shared_vars.js

--- client/src/shared_vars.js
+++ client/src/shared_vars.js
@@ -1,9 +1,26 @@
 import React from 'react';
 
+class ThemeDependentComponent extends React.Component {
+  constructor() {
+		super();
+    this.state = { data: null };
+    this.updateTheme();
+	};
+  updateTheme() {
+    const theme = shared_vars.theme;
+    import('./theme_info/'+theme).then(d => {
+      if (shared_vars.theme == theme) {
+        this.setState({ data: d.default });
+      };
+    });
+  };
+};
+
 let shared_vars = {
   "theme": "default",
   "gotoPage": {},
   "defaultLoading": <div>YARR! This be loadin'...</div>,
-  "symbols": ["rob", "kill", "present", "declareScore", "swap", "chooseNextSquare", "shield", "mirror", "goToZero", "double", "bank"]
+  "symbols": ["rob", "kill", "present", "declareScore", "swap", "chooseNextSquare", "shield", "mirror", "goToZero", "double", "bank"],
+  "ThemeDependentComponent": ThemeDependentComponent
 };
 export default shared_vars;
