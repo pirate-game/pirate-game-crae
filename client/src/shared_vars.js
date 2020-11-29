72fa0c10b5b9298ba174739631eaba38d7f8ece6
The Pirate King <60094408+pirate-game@users.noreply.github.com>
2020-11-29 11:20:35 +0000

    Update shared_vars.js

--- client/src/shared_vars.js
+++ client/src/shared_vars.js
@@ -1,15 +1,15 @@
 import React from 'react';
 
-function pushState(state) {
+/*function pushState(state) {
   window.history.pushState(state, "");
-};
+};*/
 
 let shared_vars = {
   "theme": "default",
-  "currentPlace": "index",
-  "pushState": pushState,
-  "forward": () => shared_vars.pushState(shared_vars.currentPlace),
-  "back": (ev) => { ev.preventDefault(); shared_vars.gotoPage[ev.state](); },
+  //"currentPlace": "index",
+  //"pushState": pushState,
+  //"forward": () => shared_vars.pushState(shared_vars.currentPlace),
+  //"back": (ev) => { ev.preventDefault(); shared_vars.gotoPage[ev.state](); },
   "gotoPage": {},
   "defaultLoading": <div>YARR! This be loadin'...</div>
 };
