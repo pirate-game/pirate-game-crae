a4c34108e1814454067e160b9291fcf98145256a
The Pirate King <60094408+pirate-game@users.noreply.github.com>
2020-12-01 21:03:07 +0000

    Beautified code.

--- client/src/rules.js
+++ client/src/rules.js
@@ -7,22 +7,22 @@ export default class Rules extends shared_vars.ThemeDependentComponent {
         let content = shared_vars.defaultLoading;
         const data = this.state.data;
         if (data) {
-	    content = <React.Fragment>
+            content = <React.Fragment>
                 <p>{shared_vars.intersperseWith(data.rules_preamble, shared_vars.twoNewLines)}</p>
                 <div id="symbols">
                     {shared_vars.symbols.map(symbol => {
                         const sym = data[symbol];
                         return <div>
-					<div class="square"><img src={process.env.PUBLIC_URL+"/imgs/"+shared_vars.theme+"/"+symbol+sym.file_ext} /></div>
-					<p>{sym.name+": "+sym.description}</p>
-		        	</div>;}
-                        )}
+                            <div class="square"><img src={process.env.PUBLIC_URL+"/imgs/"+shared_vars.theme+"/"+symbol+sym.file_ext} /></div>
+                            <p>{sym.name+": "+sym.description}</p>
+                        </div>;}
+                    )}
                 </div>
-                <p>{data.rules_footer}</p>
-            </React.Fragment>;
-	};
-        return <div className="sometext" style={{backgroundColor:"lightblue"}}>
-            {content}
-        </div>;
+            <p>{data.rules_footer}</p>
+        </React.Fragment>;
+    };
+    return <div className="sometext" style={{backgroundColor:"lightblue"}}>
+        {content}
+    </div>;
     };
 };
