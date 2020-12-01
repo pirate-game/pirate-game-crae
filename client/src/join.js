1e3d84f172d4183576d230dc29b30954fe3d5a6e
The Pirate King <60094408+pirate-game@users.noreply.github.com>
2020-12-01 16:34:39 +0000

    Create join.js

---
+++ client/src/join.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import shared_vars from './shared_vars';
+
+//...
+
+export default class Join extends shared_vars.ThemeDependentComponent {
+  render() {
+    return <p>Join Content</p>;
+  };
+};
