31f3251b4f2baa42b10a37290287d6de4ba86f4e
The Pirate King <60094408+pirate-game@users.noreply.github.com>
2020-12-01 16:34:58 +0000

    Create watch.js

---
+++ client/src/watch.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import shared_vars from './shared_vars';
+
+//...
+
+export default class Watch extends shared_vars.ThemeDependentComponent {
+  render() {
+    return <p>Watch Content</p>;
+  };
+};
