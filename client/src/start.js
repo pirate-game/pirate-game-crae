65914f3d347005338828e3b5de9e5460a7dbb5a5
The Pirate King <60094408+pirate-game@users.noreply.github.com>
2020-12-01 16:34:10 +0000

    Create start.js

---
+++ client/src/start.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import shared_vars from './shared_vars';
+
+//...
+
+export default class Start extends shared_vars.ThemeDependentComponent {
+  render() {
+    return <p>Start Content</p>;
+  };
+};
