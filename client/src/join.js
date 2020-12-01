53939bdae1cd55badba560af817635c2639e30da
The Pirate King <60094408+pirate-game@users.noreply.github.com>
2020-12-01 21:00:58 +0000

    Update join.js

--- client/src/join.js
+++ client/src/join.js
@@ -5,7 +5,7 @@ import shared_vars from './shared_vars';
 //...
 
 export default class Join extends shared_vars.ThemeDependentComponent {
-  render() {
-    return <p>Join Content</p>;
-  };
+    render() {
+        return <p>Join Content</p>;
+    };
 };
