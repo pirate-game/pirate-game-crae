c5ec5aed8870f61145e6cd0d17b697cd962f33c0
The Pirate King <60094408+pirate-game@users.noreply.github.com>
2020-12-01 21:05:46 +0000

    Beautified code.

--- client/src/watch.js
+++ client/src/watch.js
@@ -5,7 +5,7 @@ import shared_vars from './shared_vars';
 //...
 
 export default class Watch extends shared_vars.ThemeDependentComponent {
-  render() {
-    return <p>Watch Content</p>;
-  };
+    render() {
+        return <p>Watch Content</p>;
+    };
 };
