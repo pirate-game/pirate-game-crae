97dbaee7e9425b0426c5176f1f6677910acae5ba
The Pirate King <60094408+pirate-game@users.noreply.github.com>
2020-12-01 21:04:38 +0000

    Beautified code.

--- client/src/start.js
+++ client/src/start.js
@@ -3,21 +3,21 @@ import React from 'react';
 import shared_vars from './shared_vars';
 
 export default class Start extends shared_vars.ThemeDependentComponent {
-  componentDidMount() {
-    // socket stuff etc.
-    console.log("did mount");
-    super.componentDidMount();
-  };
-  componentWillUnmount() {
-    super.componentWillUnmount();
-    // socket stuff etc.
-    console.log("will unmount");
-  };
-  render() {
-    if (this.state.data) {
-      return <p>{this.state.data.introduction_before[0]}</p>;
-    } else {
-      return <p>Start Content</p>;
+    componentDidMount() {
+        // socket stuff etc.
+        console.log("did mount");
+        super.componentDidMount();
     };
-  };
+    componentWillUnmount() {
+        super.componentWillUnmount();
+        // socket stuff etc.
+        console.log("will unmount");
+    };
+    render() {
+        if (this.state.data) {
+            return <p>{this.state.data.introduction_before[0]}</p>;
+        } else {
+        return <p>Start Content</p>;
+        };
+    };
 };
