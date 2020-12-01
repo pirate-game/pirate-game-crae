ee7e6c798a241e8d755b5c4a41244df17eb04d83
The Pirate King <60094408+pirate-game@users.noreply.github.com>
2020-12-01 19:59:01 +0000

    Update start.js

--- client/src/start.js
+++ client/src/start.js
@@ -2,9 +2,17 @@ import React from 'react';
 
 import shared_vars from './shared_vars';
 
-//...
-
 export default class Start extends shared_vars.ThemeDependentComponent {
+  componentDidMount() {
+    // socket stuff etc.
+    console.log("did mount");
+    super.componentDidMount();
+  };
+  componentWillUnmount() {
+    super.componentWillUnmount();
+    // socket stuff etc.
+    console.log("will unmount");
+  };
   render() {
     return <p>Start Content</p>;
   };
