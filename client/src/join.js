0a8794d91317ae0e91e0666c8081e22e91aff962
The Pirate King <60094408+pirate-game@users.noreply.github.com>
2020-12-05 20:57:44 +0000

    Update join.js

--- client/src/join.js
+++ client/src/join.js
@@ -1,10 +1,31 @@
 import React from 'react';
 
+import io from 'socket.io-client';
+
 import * as shared_vars from './shared_vars';
 
-//...
+import * as GameThings from './GameThings';
 
-export default class Join extends shared_vars.ThemeDependentComponent {
+let theJoin = null;
+
+export default class Join extends React.Component {
+    constructor() {
+        super();
+        
+        this.state = {content: null};
+    };
+    componentDidMount() {
+        theJoin = this;
+        
+        this.socket = io();
+        
+        this.setState({content: null}); // last line
+    };
+    componentWillUnmount() {        
+        
+        if (this.socket.connected) this.socket.disconnect();
+        
+    };
     render() {
         return <p>Join Content</p>;
     };
