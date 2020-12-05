b6a7dc8a16c40b7cbb3625866f3e0600aa750fd8
The Pirate King <60094408+pirate-game@users.noreply.github.com>
2020-12-05 13:11:59 +0000

    Update gameThings.js

--- client/src/gameThings.js
+++ client/src/gameThings.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 
 import * as shared_vars from './shared_vars';
 
+import './css/popUps.css';
+
 
 export function sortByScore(results) {
     const out = results.slice();
@@ -55,5 +57,57 @@ export class PopUps extends React.Component {
     };
     render() {
         return <div id="popUps">{this.state.children}</div>;
+    };
+};
+
+export class Board extends React.Component {
+    constructor() {
+        super();
+        this.state = {done: []};
+    };
+    squareDone(square) {
+        this.setState(state => {
+            state.done.push(square);
+            return state;
+        });
+        return this;
+    };
+    render() {
+        return <table id="board">
+            <tr className="edge">
+                <th className="edge"></th>
+                <th className="edge">A</th>
+                <th className="edge">B</th>
+                <th className="edge">C</th>
+                <th className="edge">D</th>
+                <th className="edge">E</th>
+                <th className="edge">F</th>
+                <th className="edge">G</th>
+            </tr>
+            {["1","2","3","4","5","6","7"].map(col => (
+                <tr className="edge">
+                    <th className="edge">{col}</th>
+                    {["A","B","C","D","E","F","G"].map(row => (
+                        <td id={row+col} className="square" style={{backgroundColor:(this.state.done.includes(row+col)?"#CC6600":"white")}}></td>
+                    ))}
+                </tr>
+            ))}
+      </table>;
+    };
+};
+
+export class CurrentSquare extends React.Component {
+    constructor() {
+        super();
+        this.state = {currentSquare: "??"};
+    };
+    setCurrent(square) {
+        this.setState({currentSquare: square});
+        return this;
+    };
+    render() {
+        return <div className="currentSquare">
+            <h2>Current Square: {this.state.currentSquare}</h2>
+        </div>;
     };
 };
