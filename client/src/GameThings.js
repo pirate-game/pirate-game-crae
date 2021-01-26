90d1b38f29adc3227c62224ec393758ba216e4e2
The Pirate King <60094408+pirate-game@users.noreply.github.com>
2021-01-26 17:08:56 +0000

    Functionalised components, and optimised NiceList.

--- client/src/GameThings.js
+++ client/src/GameThings.js
@@ -12,147 +12,97 @@ export function sortByScore(results) {
     return out;
 };
 
-export class PopUps extends React.Component {
+
+export class PopUps_data {
     constructor() {
-        super();
-        
-        this.state = { children: [] };
+        this.children = [];
         
         this.clear = this.clear.bind(this);
         this.addPopUp = this.addPopUp.bind(this);
+        this.push = this.push.bind(this);
         this.pop = this.pop.bind(this);
     };
     clear() {
-        this.setState({ children: [] });
+        this.children = [];
         return this;
     };
     addPopUp(description) {
-        this.setState(state => {
-            state.elems.push(<div id={description.id} className="popUp"><div>
-                <h3>{description.title}</h3>
-                <hr />
-                <p>{shared_vars.intersperseWith(description.textLines || [], <br />)}</p>
-                {description.btn && <button className="close" onClick={description.btn.onClick}>{description.btn.text}</button>}
-            </div></div>);
-            return state;
-        });
+        this.children.push(<div id={description.id} className="popUp"><div>
+            <h3>{description.title}</h3>
+            <hr />
+            <p>{shared_vars.intersperseWith(description.textLines || [], <br />)}</p>
+            {description.btn && <button className="close" onClick={description.btn.onClick}>{description.btn.text}</button>}
+        </div></div>);
         return this;
     };
     push(e) {
-        this.setState(state => {
-            state.elems.push(e);
-            return state;
-        });
+        this.children.push(e);
         return this;
     };
     pop() {
-        this.setState(state => {
-            state.elems.pop();
-            return state;
-        });
-        return this;
-    };
-    render() {
-        return <div id="popUps">{this.state.children}</div>;
+        return this.children.pop();
     };
 };
 
-export class Board extends React.Component {
-    constructor(props) {
-        super(props);
-        this.done = props.done;
-        props.done.ref = this;
-    };
-    render() {
-        return <table id="board">
+export function PopUps(props) {
+    return <div id="popUps" style={props.style}>{props.popUps.children}</div>;
+};
+
+export function Board(props) {
+    return <table id="board" style={props.style}>
+        <tr className="edge">
+            <th className="edge"></th>
+            <th className="edge">A</th>
+            <th className="edge">B</th>
+            <th className="edge">C</th>
+            <th className="edge">D</th>
+            <th className="edge">E</th>
+            <th className="edge">F</th>
+            <th className="edge">G</th>
+        </tr>
+        {["1","2","3","4","5","6","7"].map(col => (
             <tr className="edge">
-                <th className="edge"></th>
-                <th className="edge">A</th>
-                <th className="edge">B</th>
-                <th className="edge">C</th>
-                <th className="edge">D</th>
-                <th className="edge">E</th>
-                <th className="edge">F</th>
-                <th className="edge">G</th>
+                <th className="edge">{col}</th>
+                {["A","B","C","D","E","F","G"].map(row => (
+                    <td id={row+col} className="square" style={{backgroundColor:(props.done.includes(row+col)?"#CC6600":"white")}}></td>
+                ))}
             </tr>
-            {["1","2","3","4","5","6","7"].map(col => (
-                <tr className="edge">
-                    <th className="edge">{col}</th>
-                    {["A","B","C","D","E","F","G"].map(row => (
-                        <td id={row+col} className="square" style={{backgroundColor:(this.done.includes(row+col)?"#CC6600":"white")}}></td>
-                    ))}
-                </tr>
-            ))}
-      </table>;
-    };
+        ))}
+    </table>;
 };
 
-export class CurrentSquare extends React.Component {
-    constructor() {
-        super();
-        this.state = {currentSquare: "??"};
-    };
-    setCurrent(square) {
-        this.setState({currentSquare: square});
-        return this;
-    };
-    render() {
-        return <div className="currentSquare">
-            <h2>Current Square: {this.state.currentSquare}</h2>
-        </div>;
-    };
+export function CurrentSquare(props) {
+    return <div className="currentSquare" style={props.style}>
+        <h2>Current Square: {props.square}</h2>
+    </div>;
 };
 
 const listStyle = {minHeight: '70px', maxHeight: '-webkit-fill-available', background: '#fff', 
                    overflow: 'auto', border: 'solid black 3px', borderRadius: '10px',
                    listStyleType: 'none', margin: 0, padding: 0};
 
-export class NiceList extends React.Component {
-    constructor(props) {
-        super(props);
-        
-        Object.assign(this.props.style, listStyle);
-        
-        this.state = {elems: props.initial || []};
+export class List_data {
+    constructor(initial) {
+        this.elems = initial || [];
     };
     push(e) {
-        this.setState(state => {
-            state.elems.push(e);
-            return state;
-        });
+        this.elems.unshift(e); // pushes to front
+        return this;
     };
     remove(e) {
-        this.setState(state => {
-            shared_vars.removeFirstOccurrenceIn(e, state.elems);
-            return state;
-        });
-    };
-    render() {
-        return <ul style={this.props.style}>
-            {this.state.elems.slice().reverse().map((e, pos) => (
-                <li style={{position:'relative', padding: '5px 10px'}}>
-                    <div style={{background: ((pos % 2) ? '#eee' : '#fff'), width: 'calc(100% - 30px)', overflowWrap: 'break-word'}}>{e}</div>
-                </li>
-          ))}
-        </ul>;
+        shared_vars.removeFirstOccurrenceIn(e, this.elems);
+        return this;
     };
 };
 
-export class ListWithCrosses extends NiceList {
-    constructor(props) {
-        super(props);
-
-        this.callback = this.props.callback.bind(this);
-    };
-    render() {
-        return <ul style={this.props.style}>
-            {this.state.elems.slice().reverse().map((e, pos) => (
-                <li style={{position:'relative', padding: '5px 10px'}}>
-                    <div style={{display: 'inline-block', float: 'right', fontSize: '40px', fontWeight: 'bold', padding: '10px', cursor: 'pointer',
-                                position: 'absolute', right: 0, top: '-21px'}} onClick={() => this.callback(e, pos)}>&times;</div>
-                    <div style={{background: ((pos % 2) ? '#eee' : '#fff'), width: 'calc(100% - 30px)', overflowWrap: 'break-word'}}>{e}</div>
-                </li>
-          ))}
-        </ul>;
-    };
+export function NiceList(props) {
+    return <ul style={props.style}>
+        {props.elems.map((e, pos) => (
+            <li style={{position:'relative', padding: '5px 10px'}}>
+                {props.callback && <div style={{display: 'inline-block', float: 'right', fontSize: '40px', fontWeight: 'bold', padding: '10px', cursor: 'pointer',
+                                                position: 'absolute', right: 0, top: '-21px'}} onClick={() => props.callback(e, pos)}>&times;</div>}
+                <div style={{background: ((pos % 2) ? '#eee' : '#fff'), width: 'calc(100% - 30px)', overflowWrap: 'break-word'}}>{e}</div>
+            </li>
+        ))}
+    </ul>;
 };
