61a1b7e4c01c26625659c28d770427ef24133866
The Pirate King <60094408+pirate-game@users.noreply.github.com>
2021-02-02 17:06:08 +0000

    Boilerplate for Watch component.

--- client/src/watch.js
+++ client/src/watch.js
@@ -2,10 +2,29 @@ import React from 'react';
 
 import * as shared_vars from './shared_vars';
 
-//...
+import * as GameThings from './GameThings';
 
-export default class Watch extends shared_vars.ThemeDependentComponent {
-    render() {
-        return <p>Watch Content</p>;
+import './css/watch.css';
+
+
+export default class Watch extends GameThings.SocketfulComponent {
+    constructor() {
+        super();
+        Object.assign(this.state, {/* ... */});
+        this.outerName = "watchContent";
+    };
+    componentDidMount() {
+        super.componentDidMount(); // first line
+        
+        // this.socket.on(...)...;
+        
+        this.setState({stage: 0}); // last line
+    };
+    // add back componentWillUnmount in unlikely event that stage must be reset
+    render_helper(data, stage) {
+        switch (stage) {
+            default: return null;
+            case 0: return "Watch content!";
+        };
     };
 };
