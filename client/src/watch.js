41c36fc7aafa5ca7e3d33f33bb1f1df6a0f9db4b
The Pirate King <60094408+pirate-game@users.noreply.github.com>
2021-02-02 17:34:12 +0000

    Added most of stage 0.

--- client/src/watch.js
+++ client/src/watch.js
@@ -12,11 +12,26 @@ export default class Watch extends GameThings.SocketfulComponent {
         super();
         Object.assign(this.state, {/* ... */});
         this.outerName = "watchContent";
+        
+        this.attemptWatch = this.attemptWatch.bind(this);
     };
     componentDidMount() {
         super.componentDidMount(); // first line
         
-        // this.socket.on(...)...;
+        this.socket.on('no_such_game', () => {
+            document.getElementById("gameKey").value = "";
+            this.remove_popUp();
+            this.add_popUp({
+                title: "No Such Game",
+                textLines: ["You've probably mistyped your key. Try again."],
+                btn: this.default_btn
+            });
+        });
+        
+        this.socket.on('start_game', () => {
+            this.remove_popUp();
+            this.setState({stage: 2}); // there is no stage 1 for Watch
+        });
         
         this.setState({stage: 0}); // last line
     };
@@ -24,7 +39,26 @@ export default class Watch extends GameThings.SocketfulComponent {
     render_helper(data, stage) {
         switch (stage) {
             default: return null;
-            case 0: return "Watch content!";
+            case 0: return <div>
+                <h2 style={{marginTop: 0}}>{data.watchQ}</h2>
+                <input type="text" id="gameKey" maxLength="6" />
+                <button id="watch" onClick={this.attemptWatch}>Watch</button>
+            </div>;
+            case 2: return "GAME STARTED!";
+        };
+    };
+    attemptWatch() {
+        const key = document.getElementById("gameKey").value;
+        this.remove_popUp();
+        if (shared_vars.keyPattern.test(key)) {
+            this.push_popUp(GameThings.waitingPopUp);
+            this.socket.emit('attempt_watch', key);
+        } else {
+            this.add_popUp({
+                title: "Invalid Key",
+                textLines: ['Please correctly type the key you have been given. It should be 6 hexadecimal digits e.g. "face42".'],
+                btn: this.default_btn
+            });
         };
     };
 };
