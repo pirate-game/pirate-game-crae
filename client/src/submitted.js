118c9f4c8e7162afe4d8e014ac45055440836ab6
The Pirate King <60094408+pirate-game@users.noreply.github.com>
2021-02-03 16:00:20 +0000

    Removed redundant style and import.

--- client/src/submitted.js
+++ client/src/submitted.js
@@ -1,10 +1,8 @@
 import React from 'react';
 
-import * as shared_vars from './shared_vars';
-
 export default class Submitted extends React.Component {
     render() {
-        return <div className="sometext" style={{backgroundColor:"lightblue"}}>
+        return <div className="sometext">
             <p>Thank you! Form submitted. Hopefully your form made it to us.</p>
         </div>;
     };
