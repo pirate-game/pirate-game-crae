14386ddd4907b578801f1b249b1cdc1942445465
The Pirate King <60094408+pirate-game@users.noreply.github.com>
2021-02-16 14:50:08 +0000

    Added display of errors in form submission.

--- client/src/submitted.js
+++ client/src/submitted.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
+function getError() {
+    const pos = window.location.href.location("error=");
+    if (pos === -1) return false;
+    const end = window.location.href.location("&", pos); // starts from pos
+    if (end === -1) return window.location.href.slice(pos + 6); // rest of string
+    return window.location.href.slice(pos + 6, end); // rest of string up to end
+};
+
 export default class Submitted extends React.Component {
     render() {
         return <div className="sometext">
-            <p>Thank you! Form submitted. Hopefully your form made it to us.</p>
+            <p>{getError() || "Thank you! Form submitted. Hopefully your form made it to us."}</p>
         </div>;
     };
 };
